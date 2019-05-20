b10ef18553c64c2102ab0bbb8edec34750f47e04
gauravmahto <gauravm.git@gmail.com>
2019-05-20 17:22:10 +0530

    Updated the Win10SDK target.

--- CPP-New-Standards/PrefixSums.cpp
+++ CPP-New-Standards/PrefixSums.cpp
@@ -16,7 +16,7 @@ namespace codility
 			long totalPairs = 0;
 			long maxPairs = 1000000000;
 			std::vector<int> sumAt(vec.size() + 1);
-			int index = 1;
+			size_t index = 1;
 
 			for (const auto val : vec)
 			{
@@ -48,12 +48,60 @@ namespace codility
 
 		}
 
-		std::vector<int> FindDNAMinimumImpact(std::wstring& /*str*/, std::vector<int>& /*seqPOSStart*/, std::vector<int>& seqPOSEnd)
+		std::vector<int> FindDNAMinimumImpact(std::wstring & str, std::vector<int> & seqPOSStart, std::vector<int> & seqPOSEnd)
 		{
 
-			std::wcout << L"TODO - Implement - FindDNAMinimumImpact()";
+			const auto getFactor = [](const wchar_t& letter) -> int {
+				switch (letter) {
+				case L'A':
+					return 1;
+				case L'C':
+					return 2;
+				case L'G':
+					return 3;
+				case L'T':
+					return 4;
+				default:
+					return 0;
+				}
+			};
+			const std::map<wchar_t, int> dnaNucleotideFactor{
+				{L'A', 1},
+				{L'C', 2},
+				{L'G', 3},
+				{L'T', 4}
+			};
+			std::vector<int> strPrefixSum(str.length() + 1);
 
-			return seqPOSEnd;
+			{
+				size_t index = 1;
+
+				for (auto itr = str.begin(); itr != str.end(); ++itr, ++index)
+				{
+
+					strPrefixSum[index] = (strPrefixSum[index - 1] + getFactor(*itr));
+
+				}
+
+			}
+
+			{
+
+				int index = 0;
+
+				for (auto startPOSItr = seqPOSStart.begin(); startPOSItr != seqPOSStart.end(); ++startPOSItr, ++index)
+				{
+
+					auto range = std::make_pair(*startPOSItr, seqPOSEnd[index]);
+
+					// TODO - continue
+
+				}
+
+			}
+
+
+			return strPrefixSum;
 
 		}
 
